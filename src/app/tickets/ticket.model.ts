1730276723270bf19c3ad18239dc5285279bd37a
Julia Dominguez <juliamdominguez@gmail.com>
2020-04-21 14:55:59 -0400

    changes to ticket-list and header, added fxLayout

--- src/app/tickets/ticket.model.ts
+++ src/app/tickets/ticket.model.ts
@@ -1,9 +1,23 @@
-// import { KTool } from '../shared/k-tool.model';
+import { KTool } from '../shared/k-tool.model';
+export class Ticket {
+  // this form will be the 20+ fields
+  public name: string;
+  public description: string;
+  public imagePath: string;
+  public ktools: KTool[];
+  public ticketNumber: string;
 
-export interface Ticket {
-  id: string;
-  title: string;
-  description: string;
-  ticketNumber: string;
-  // ktool: KTool;
+  constructor(
+    name: string,
+    desc: string,
+    imagePath: string,
+    ktools: KTool[],
+    ticketNumber: string
+  ) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+    this.ktools = ktools;
+    this.ticketNumber = ticketNumber;
+  }
 }

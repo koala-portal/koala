eb7c0ebc3d071ab241f24b8702b920abb436a403
Julia Dominguez <juliamdominguez@gmail.com>
2020-04-24 20:25:33 -0400

    final cleanup

--- src/app/abode/abode.component.ts
+++ src/app/abode/abode.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-abode',
-  templateUrl: './abode.component.html',
-  styleUrls: ['./abode.component.scss'],
+  selector: "app-abode",
+  templateUrl: "./abode.component.html",
+  styleUrls: ["./abode.component.scss"]
 })
-export class AbodeComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  // nothing yet
-}
+export class AbodeComponent {}

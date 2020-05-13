51cfeca61fd2f91c9830b727149d38c6afa220a1
MATTHEW AZUKAS <mazukas@gmail.com>
2020-05-13 14:15:56 -0400

    Updated ReadMe file on how to build and deploy.

--- src/main/java/com/koala/portal/PortalApplication.java
+++ src/main/java/com/koala/portal/PortalApplication.java
@@ -129,17 +129,5 @@ public class PortalApplication {
 			e.printStackTrace();
 		}
 	}
-
-	/*
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200").allowCredentials(true);
-			}
-		};
-	}
-	*/
 	
 }

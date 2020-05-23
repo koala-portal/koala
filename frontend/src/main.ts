983454fff6e7e47f53fe5413c95e8fb2d7b3622e
MATTHEW AZUKAS <mazukas@gmail.com>
2020-05-23 16:30:57 -0400

    Checked in something that should have been commented out.  Sorry about that.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import {LicenseManager} from '../node_modules/ag-grid-enterprise/dist/lib/licenseManager';
-LicenseManager.setLicenseKey("For_Trialing_ag-Grid_Only-Not_For_Real_Development_Or_Production_Projects-Valid_Until-18_July_2020_[v2]_MTU5NTAyNjgwMDAwMA==f73bd8ca97817c0fd47ae9b39bdeb3d6");
+//import {LicenseManager} from '../node_modules/ag-grid-enterprise/dist/lib/licenseManager';
+//LicenseManager.setLicenseKey("For_Trialing_ag-Grid_Only-Not_For_Real_Development_Or_Production_Projects-Valid_Until-18_July_2020_[v2]_MTU5NTAyNjgwMDAwMA==f73bd8ca97817c0fd47ae9b39bdeb3d6");
 
 if (environment.production) {
   enableProdMode();
